b4a51e80750475a94add1e298c0075f1937fec1c
Nitish Kafle <kaflenit@gmail.com>
2022-04-04 21:46:51 -0400

    relative path fixed for imports

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,17 +1,23 @@
 import { Entity, OptionalProps, PrimaryKey, Property } from "@mikro-orm/core";
+import { Field, Int, ObjectType } from "type-graphql";
 
+@ObjectType()
 @Entity()
 export class Post {
   [OptionalProps]?: "title";
+  @Field(() => Int)
   @PrimaryKey()
   id!: number;
 
-  @Property({ type: "date", default: "NOW()" })
+  @Field(() => String)
+  @Property({ type: "date" })
   createdAt = new Date();
 
+  @Field(() => String)
   @Property({ type: "date", onUpdate: () => new Date() })
   updateAt = new Date();
 
+  @Field()
   @Property({ type: "text" })
   title!: string;
 }
